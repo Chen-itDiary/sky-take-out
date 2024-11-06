7c79f73a553c7632a91d10e22b3e5bfc7ac7b63e
陈亦让 <321289707@qq.com>
2024-11-06 20:15:48 +0800

    测试

--- sky-common/src/main/java/com/sky/properties/JwtProperties.java
+++ sky-common/src/main/java/com/sky/properties/JwtProperties.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 @Component
+
 @ConfigurationProperties(prefix = "sky.jwt")
 @Data
 public class JwtProperties {
@@ -19,5 +20,4 @@ public class JwtProperties {
     private String userSecretKey;
     private long userTtl;
     private String userTokenName;
-
 }
