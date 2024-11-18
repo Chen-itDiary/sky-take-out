014da0d69dce5031530696bacb12e3fae8cb5d86
Chen-itDiary <321289707@qq.com>
2024-11-18 15:54:11 +0800

    新增菜品业务代码开发

---
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -0,0 +1,55 @@
+package com.sky.service.impl;
+
+import com.fasterxml.jackson.databind.util.BeanUtil;
+import com.sky.dto.DishDTO;
+import com.sky.entity.Dish;
+import com.sky.entity.DishFlavor;
+import com.sky.mapper.DishFlavorMapper;
+import com.sky.mapper.DishMapper;
+import com.sky.service.DishService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Slf4j
+public class DishServiceImpl implements DishService {
+
+    @Autowired
+    private DishMapper dishMapper;
+    @Autowired
+    private DishFlavorMapper dishFlavorMapper;
+
+
+    /*
+     * 新增菜品和对应的口味数据
+     * @param dishDTO
+     * */
+
+    @Transactional//关联到了两张表，因此要开启事务，要么全成功，要么全失败
+    @Override
+    public void saveWithFlavor(DishDTO dishDTO) {
+
+        Dish dish = new Dish();
+        BeanUtils.copyProperties(dishDTO,dish);
+
+        //向菜品表插入1条数据
+        dishMapper.insert(dish);
+        //获取生成的菜品id
+        Long id = dish.getId();
+
+        //向口味表插入n条数据
+        List<DishFlavor> flavors = dishDTO.getFlavors();
+        if(flavors != null && !flavors.isEmpty()){
+            flavors.forEach(dishFlavor -> {
+                dishFlavor.setDishId(id);//先将菜品的id传入到每个flavor的dishId属性中
+            });
+            dishFlavorMapper.insertBatch(flavors);
+        }
+
+    }
+}
